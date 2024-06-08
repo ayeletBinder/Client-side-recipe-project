600cb70116ce87bef297614842031831dc018f85
ayeletBinder <ayeletbinder@gmail.com>
2024-06-09 02:54:37 +0300

    star- pipe

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -3,11 +3,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../../shared/services/recipes.service';
 import { Recipe } from '../../shared/models/recipe';
 import { LucideAngularModule, CookingPot } from 'lucide-angular';
+import { StarsDifficultyLevelPipe } from '../../shared/pipes/stars-difficulty-level.pipe';
 
 @Component({
   selector: 'app-recipe-details',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule,StarsDifficultyLevelPipe],
   templateUrl: './recipe-details.component.html',
   styleUrl: './recipe-details.component.scss',
 })
@@ -16,7 +17,7 @@ export class RecipeDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
   recipeService = inject(RecipesService);
   recipe: Recipe = {};
-  products: string[]=[];
+  products: string[][]=[];
   instructions: string[]=[];
 
   ngOnInit() {
@@ -24,19 +25,28 @@ export class RecipeDetailsComponent implements OnInit {
     console.log(idRecipe);
     this.recipeService.getRecipeById(idRecipe)?.subscribe((data) => {
       this.recipe = data as any;
-      console.log(this.recipe);
+      console.log("eee",this.recipe);
     });
 
   }
-  productsFunction() {
-      this.recipe.layers?.forEach(element => {
-      element.products?.split(' ');
+  j(){
+    this.recipe.layers?.forEach((layer) => {
+      if (layer.products) {
+        this.products.push( layer.products.split(" ")); 
+      }
     });
-    console.log(this.recipe.layers,"this.recipe.layers");
+    console.log("oo",this.products);
   }
-  instructionsFunction() {
-    this.recipe.instructions?.split('.');
-    console.log(this.recipe.instructions,"this.recipe.instructions");
+  p(){
+    if(this.recipe.instructions!=undefined){
+      debugger
+      this.instructions=this.recipe.instructions?.split("."); 
+      this.instructions.splice(this.instructions.length-1,1); 
+      console.log('u',this.instructions);
+    }
+    else{
+      console.log("oooooooooooooooo",this.recipe.instructions);
+      
+    }
   }
-    
 }
